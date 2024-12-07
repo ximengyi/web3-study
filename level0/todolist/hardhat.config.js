e1c3f8226dcd3daba9ea199e99e6a0b6f02f4374
ximengyi <1203669692@qq.com>
2024-12-07 21:42:03 +0800

    todolist

---
+++ level0/todolist/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = { 
+solidity: "0.8.28", 
+networks: { 
+hardhat: {}, 
+
+sepolia: { 
+url:
+ "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY, accounts: [`0x${process.env.PRIVATE_KEY}`], 
+ }, 
+}, 
+};
